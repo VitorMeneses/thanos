b280962e722192bac5f4b42eb6da359aaf7902b4
vitormeneses <vitorramiloks@gmail>
2018-12-25 23:17:26 -0200

    new app inclusion

--- thanos/settings.py
+++ thanos/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'spaceBlue',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'BRST'
+TIME_ZONE = 'America/Recife'
 
 USE_I18N = True
 
